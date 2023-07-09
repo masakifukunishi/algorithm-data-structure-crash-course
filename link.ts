da2fb9f74d41cbd1646e177ee5e55be80ca5d1b0
MasakiFukunishi <fmsknn@gmail.com>
2023-07-09 10:11:09 -0700

    Fix constructor

--- link.ts
+++ link.ts
@@ -9,14 +9,14 @@ class LinkedListNode {
 }
 
 class MyLinkedList {
-  private head: LinkedListNode;
-  private tail: LinkedListNode;
+  private head: LinkedListNode | null;
+  private tail: LinkedListNode | null;
   private length: number;
 
-  constructor(val: number) {
-    this.head = new LinkedListNode(val);
-    this.tail = this.head;
-    this.length = 1;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   get(index: number): number {
@@ -24,33 +24,54 @@ class MyLinkedList {
       return -1;
     }
     const leader = this.traverseToIndex(index);
-    return leader.val!;
+    return leader!.val!;
   }
 
   addAtHead(val: number): void {
     const node = new LinkedListNode(val);
-    node.next = this.head;
-    this.head = node;
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+
     this.length++;
   }
 
   addAtTail(val: number): void {
     const node = new LinkedListNode(val);
-    this.tail.next = node;
-    this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail!.next = node;
+      this.tail = node;
+    }
     this.length++;
   }
 
   addAtIndex(index: number, val: number): void {
-    if (index < 0 || index >= this.length) {
+    if (index < 0 || index > this.length) {
+      return;
+    }
+    if (index === 0) {
+      this.addAtHead(val);
+      return;
+    }
+
+    if (index === this.length) {
+      this.addAtTail(val);
       return;
     }
 
     const newNode = new LinkedListNode(val);
     const leader = this.traverseToIndex(index - 1);
-    const holdingPointer = leader.next;
-    leader.next = newNode;
+    const holdingPointer = leader!.next;
+    leader!.next = newNode;
     newNode.next = holdingPointer;
+    this.length++;
     return;
   }
 
@@ -59,14 +80,25 @@ class MyLinkedList {
       return;
     }
 
-    const leader = this.traverseToIndex(index - 1);
-    const unwantedNode = leader.next;
-    leader.next = unwantedNode!.next;
+    if (index === 0) {
+      this.head = this.head!.next;
+      if (this.head === null) {
+        this.tail = null;
+      }
+    } else {
+      const leader = this.traverseToIndex(index - 1);
+      const unwantedNode = leader!.next;
+      leader!.next = unwantedNode!.next;
+      if (unwantedNode === this.tail) {
+        this.tail = leader;
+      }
+    }
+
     this.length--;
   }
 
-  private traverseToIndex(index: number): LinkedListNode {
-    let currentNode: LinkedListNode = this.head;
+  private traverseToIndex(index: number): LinkedListNode | null {
+    let currentNode: LinkedListNode | null = this.head;
     let counter = 0;
     while (currentNode !== null && counter !== index) {
       currentNode = currentNode.next!;
@@ -76,16 +108,6 @@ class MyLinkedList {
   }
 }
 
-const linkedList = new MyLinkedList(2);
-console.log(linkedList);
-linkedList.addAtHead(1);
-console.log(linkedList);
-linkedList.addAtTail(3);
-console.log(linkedList);
-linkedList.addAtIndex(2, 4);
-console.log(linkedList);
-linkedList.deleteAtIndex(1);
-console.log(linkedList);
 /**
  * Your MyLinkedList object will be instantiated and called as such:
  * var obj = new MyLinkedList()
